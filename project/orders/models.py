05a580c0a24c1374449cd37aa70f12066570ac44
Mahoni <mahoni1983@mail.ru>
2022-03-25 21:13:35 +0200

    Мини-курс Django, Часть 5

--- project/orders/models.py
+++ project/orders/models.py
@@ -1,7 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+from products.models import Product
+
 
 class SalesOrder(models.Model):
     amount = models.IntegerField()
     description = models.CharField(max_length=255)
     date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    products = models.ManyToManyField(Product)
 # Create your models here.
